22b4ffcb12d6024a659191bc97ca5ec4fb01a196
Julien DUMAY <judumay@student.42.fr>
2019-03-04 14:45:06 +0100

    SAFE

--- srcs/ps/ft_choice.c
+++ srcs/ps/ft_choice.c
@@ -6,7 +6,7 @@
 /*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 11:17:23 by judumay           #+#    #+#             */
-/*   Updated: 2019/03/04 11:53:24 by judumay          ###   ########.fr       */
+/*   Updated: 2019/03/04 12:04:24 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ t_checke	*ft_choice(t_pile *pile)
 	ft_init_begin(&begin, pile->a, pile->b, comp);
 	ft_free_lst(temp);
 	ft_quicksort(pile, &begin, &comp, finish);
-	ft_free_lst_ps(comp);
 	pile->b ? ft_free_lst(pile->b) : 0;
 	ft_free_lst(finish);
 	return (begin.begina);
--- srcs/ps/ft_quicksort.c
+++ srcs/ps/ft_quicksort.c
@@ -6,43 +6,94 @@
 /*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 15:40:01 by lramard           #+#    #+#             */
-/*   Updated: 2019/03/04 11:58:54 by judumay          ###   ########.fr       */
+/*   Updated: 2019/03/04 14:44:01 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_push_swap.h>
 
+void		ft_tej_de_a(t_pile *pile, t_begin *begin, t_ps **comp, int n)
+{
+	t_checke	*temp;
+	int			j;
+	int			i;
+
+	j = (*comp)->nbblock + 1;
+	(*comp)->next = (t_ps*)malloc(sizeof(t_ps) * 1);
+	while (n > 3)
+	{
+		*comp = (*comp)->next;
+		temp = ft_lstndup(pile->a, n);
+		ft_init_comp(comp, temp, j);
+		ft_free_lst(temp);
+		i = -1;
+		while (++i < n)
+		{
+			if ((pile->a)->n <= (*comp)->median && ++(*comp)->size)
+			{
+				ft_pb(&pile->a, &pile->b, &begin->begina, &begin->beginb);
+				n--;
+				i--;
+			}
+			else
+				ft_ra(&pile->a, &begin->begina, 1);
+		}
+		while (i-- > 0)
+			ft_rra(&pile->a, &begin->begina, 1);
+		j++;
+	}
+	(*comp)->next = NULL;
+}
+
 void		ft_size4(t_pile *pile, t_begin *begin, t_ps **comp)
 {
-	(void)pile;
-	(void)begin;
-	(void)comp;
+	int			i;
+	t_checke	*temp;
+	int			new_size;
+
+	new_size = 0;
+	temp = ft_lstndup(begin->beginb, (*comp)->size);
+	(*comp)->median = ft_median(temp, (*comp)->median);
+	i = -1;
+	while (++i < (*comp)->size)
+	{
+		if ((pile->b)->n >= (*comp)->median)
+		{
+			ft_pa(&pile->a, &pile->b, &begin->begina, &begin->beginb);
+			new_size++;
+			(*comp)->size--;
+			i--;
+		}
+		else
+			ft_rb(&pile->b, &begin->beginb, 1);
+	}
+	while (i-- > 0)
+		ft_rrb(&pile->b, &begin->beginb, 1);
+	ft_tej_de_a(pile, begin, comp, new_size);
 }
 
 void		ft_suite_algo(t_pile *pile, t_begin *begin, t_ps **comp,
 	t_checke *finish)
 {
-	int j = 4;
 	t_ps	*memo;
 
-	while (j-- > 0 && !(ft_lst_compare(finish, pile->a)))
+	while (!(ft_lst_compare(finish, pile->a)))
 	{
 		*comp = begin->beginc;
 		while (*comp && (*comp)->next && (memo = *comp))
 			*comp = (*comp)->next;
-		ft_printf("|A "); ft_display(begin->begina, begin->beginc); ft_printf("|B "); ft_display(begin->beginb, NULL);ft_printf("|FINISH "); ft_display(finish, NULL);
 		if ((*comp)->size <= 3)
 		{
 			begin->beginb->next && begin->beginb->n < begin->beginb->next->n
 				? ft_sb(&pile->b, &begin->beginb, 1) : 0;
 			while ((*comp)->size-- > 0)
 				ft_pa(&pile->a, &pile->b, &begin->begina, &begin->beginb);
-			ft_sort3(&pile->a, &begin->begina);
 			memo->next = NULL;
 			free(*comp);
 		}
 		else
 			ft_size4(pile, begin, comp);
+		ft_sort3(&pile->a, &begin->begina);
 	}
 }
 
@@ -68,13 +119,9 @@ t_checke	*ft_quicksort(t_pile *pile, t_begin *begin, t_ps **comp,
 			ft_free_lst(temp);
 		}
 	}
-	free((*comp)->next);
+	//free((*comp)->next);
 	(*comp)->next = NULL;
 	begin->begina = ft_3fast(&pile->a, begin);
 	ft_suite_algo(pile, begin, comp, finish);
 	return (begin->begina);
 }
-
-/*
-** 1 trop loin
-*/
